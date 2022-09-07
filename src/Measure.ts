4b7fa74ffd2d10c86a9938852fed14d2dd102f84
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-09-07 14:56:47 -0500

    change CMS ID to non required

--- src/Measure.ts
+++ src/Measure.ts
@@ -44,7 +44,7 @@ export interface Group {
 export interface Measure {
   id: string;
   versionId: string;
-  cmsId: string;
+  cmsId?: string;
   active: boolean;
   measureHumanReadableId: string;
   measureSetId: string;
