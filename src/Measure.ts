d03e081104b0feab6a59ce8b713fa4ecb41c7b1b
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-02-08 11:14:29 -0600

    change to export type to avoid empty interface

--- src/Measure.ts
+++ src/Measure.ts
@@ -81,7 +81,7 @@ export interface SupplementalData {
   description?: string;
 }
 
-export interface RiskAdjustment extends SupplementalData {}
+export type RiskAdjustment = SupplementalData;
 
 export interface Measure {
   id: string;
