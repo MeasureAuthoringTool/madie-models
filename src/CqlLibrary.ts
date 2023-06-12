619290b978d1c6b5c62fbe99fd1c7d0a3a9f5d10
mcmcphillips <matt.mcphillips@semanticbits.com>
2023-06-12 11:59:27 -0700

    update cql library types

--- src/CqlLibrary.ts
+++ src/CqlLibrary.ts
@@ -1,9 +1,18 @@
 import { Model } from "./Model";
+import { Acl } from "./Measure";
 import {ProgramUseContext} from "./ProgramUseContext";
+
+export interface LibrarySet {
+  id: string;
+  librarySetId: string;
+  owner: string;
+  acls?: Array<Acl>;
+}
 
 export interface CqlLibrary {
   id: string;
   cqlLibraryName: string;
+  librarySetId: string;
   model: Model | "";
   cqlErrors: boolean;
   cql: string;
@@ -17,5 +26,6 @@ export interface CqlLibrary {
   publisher?: string;
   description?: string;
   experimental?: boolean;
+  librarySet?: LibrarySet;
   programUseContext?: ProgramUseContext;
 }
