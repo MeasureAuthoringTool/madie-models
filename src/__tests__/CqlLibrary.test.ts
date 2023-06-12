0adf53b412fe4460f8e9ded55543b30e481b658f
mcmcphillips <matt.mcphillips@semanticbits.com>
2023-06-12 12:07:31 -0700

    Add tests

--- src/__tests__/CqlLibrary.test.ts
+++ src/__tests__/CqlLibrary.test.ts
@@ -4,6 +4,7 @@ import { Model } from "../Model";
 test("Verifies that Cql Library has the correct attributes", () => {
   const cqlLibrary: CqlLibrary = {
     id: "test",
+    librarySetId: "1",
     cqlLibraryName: "TestingCqlLibrary",
     model: Model.QICORE,
     cqlErrors: false,
@@ -18,8 +19,16 @@ test("Verifies that Cql Library has the correct attributes", () => {
     publisher: "",
     description: "",
     experimental: false,
+    librarySet: {
+      id:"2",
+      librarySetId: "1",
+      owner: "",
+      acls: []
+    }
   };
 
   expect(cqlLibrary.id).toEqual("test");
+  expect(cqlLibrary.librarySetId).toEqual("1")
+  expect(cqlLibrary.librarySet?.librarySetId).toEqual("1")
   expect(cqlLibrary.cqlLibraryName).toEqual("TestingCqlLibrary");
 });
