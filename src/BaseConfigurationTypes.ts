f9be8738c4ea57d4629f9d4031ba1c929c034405
Prateek Keerthi <prateekkeerthi7@gmail.com>
2024-06-28 09:50:30 -0400

    MAT-7231 update measure type oiptions in the UI

--- src/BaseConfigurationTypes.ts
+++ src/BaseConfigurationTypes.ts
@@ -1,10 +1,10 @@
 export enum BaseConfigurationTypes {
   APPROPRIATE_USE_PROCESS = "Appropriate Use Process",
-  COST_OR_RESOURCE_USE = "Cost/Resource Use",
+  RESOURCE_USE = "Resource Use",
   EFFICIENCY = "Efficiency",
   INTERMEDIATE_CLINICAL_OUTCOME = "Intermediate Clinical Outcome",
   OUTCOME = "Outcome",
-  PATIENT_ENGAGEMENT_OR_EXPERIENCE = "Patient Engagement/Experience",
+  EXPERIENCE = "Experience",
   PATIENT_REPORTED_OUTCOME_PERFORMANCE = "Patient Reported Outcome Performance",
   PROCESS = "Process",
   STRUCTURE = "Structure",
