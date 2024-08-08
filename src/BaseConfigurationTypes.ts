8963d127885e51d114761217ba158b60c75df55a
mcmcphillips <84744653+mcmcphillips@users.noreply.github.com>
2024-08-08 11:40:54 -0700

    Update src/BaseConfigurationTypes.ts
    Co-authored-by: Ashok Dongare <ashok.dongare@semanticbits.com>

--- src/BaseConfigurationTypes.ts
+++ src/BaseConfigurationTypes.ts
@@ -2,7 +2,7 @@ export enum BaseConfigurationTypes {
   APPROPRIATE_USE_PROCESS = "Appropriate Use Process",
   RESOURCE_USE = "Resource Use",
   EFFICIENCY = "Efficiency",
-  INTERMEDIATE_CLINICAL_OUTCOME = "Intermediate Outcome",
+  INTERMEDIATE_OUTCOME = "Intermediate Outcome",
   OUTCOME = "Outcome",
   EXPERIENCE = "Experience",
   PATIENT_REPORTED_OUTCOME_PERFORMANCE = "Patient Reported Outcome",
