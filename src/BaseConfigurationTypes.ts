61ce8b65b74a1501e5950d231a6bf0f0e2fc3552
mcmcphillips <matt.mcphillips@icf.com>
2024-08-08 08:25:34 -0700

    MAT-7492: Update typing displays

--- src/BaseConfigurationTypes.ts
+++ src/BaseConfigurationTypes.ts
@@ -2,10 +2,10 @@ export enum BaseConfigurationTypes {
   APPROPRIATE_USE_PROCESS = "Appropriate Use Process",
   RESOURCE_USE = "Resource Use",
   EFFICIENCY = "Efficiency",
-  INTERMEDIATE_CLINICAL_OUTCOME = "Intermediate Clinical Outcome",
+  INTERMEDIATE_CLINICAL_OUTCOME = "Intermediate Outcome",
   OUTCOME = "Outcome",
   EXPERIENCE = "Experience",
-  PATIENT_REPORTED_OUTCOME_PERFORMANCE = "Patient Reported Outcome Performance",
+  PATIENT_REPORTED_OUTCOME_PERFORMANCE = "Patient Reported Outcome",
   PROCESS = "Process",
   STRUCTURE = "Structure",
 }
