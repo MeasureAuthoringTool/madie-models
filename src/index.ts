7571de3e83d49e67420564e7f19f1c8db9e922ca
Prateek Keerthi <prateekkeerthi7@gmail.com>
2025-12-09 11:13:55 -0600

    MAT-9097 fixed export

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,4 @@ export * from "./OverlappingCodeDto";
 export * from "./OwnershipType";
 export * from "./MeasureSearchCriteria";
 export * from "./MeasureHistoryActions";
+export * from "./UserDetails";
