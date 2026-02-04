ddb1dfc9ef365dcdc6a809d8c37b700db717d7da
nmorasb <nate.moraca@semanticbits.com>
2026-02-04 15:27:20 -0500

    MAT-9658: add user login dtos

--- src/UserDetails.ts
+++ src/UserDetails.ts
@@ -4,3 +4,20 @@ export interface UserDetails {
   lastName?: string;
   email?: string;
 }
+
+export enum UserStatus {
+  ACTIVE,
+  DEACTIVATED,
+  ERROR_SUSPENDED
+}
+
+export interface HarpRole {
+  roleType?: string;
+  role?: string;
+}
+
+export interface UserLogin {
+  harpId?: string;
+  status?: UserStatus;
+  roles: HarpRole[];
+}
